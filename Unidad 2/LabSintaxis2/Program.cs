6ec36f181ede262253582113e7412b0ee0fe3604
Franco Villegas <villegasfranco1098@gmail.com>
2020-04-12 18:54:01 -0300

    Unidad 1 y 2

---
+++ Unidad 2/LabSintaxis2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un texto");
+            string inputTexto = Console.ReadLine();
+
+            if (inputTexto != "")
+            {
+                muestraMenu(inputTexto);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("¡ERROR! Debe ingresar un texto.");
+            }
+            Console.ReadKey();
+        }
+
+        private static void muestraMenu(string inputTexto)
+        {
+            Console.Clear();
+
+            Console.Write("Ingrese una opcion:" + Environment.NewLine +
+                "1- Mostrar en mayúsculas" + Environment.NewLine +
+                "2- Mostrar en minúsculas" + Environment.NewLine +
+                "3- Mostrar cantidad de caracteres" + Environment.NewLine);
+
+            ConsoleKeyInfo opcion = Console.ReadKey();
+
+            if (esOpcionValida(opcion))
+            {
+                muestraRespuestaMenu(opcion, inputTexto);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("¡ERROR! Debe ingresar una opción válida.");
+            }
+        }
+
+        private static bool esOpcionValida(ConsoleKeyInfo opcion)
+        {
+            return opcion.Key == ConsoleKey.D1 || opcion.Key == ConsoleKey.NumPad1 || // verifico numpad 1 y 1 comun
+                opcion.Key == ConsoleKey.D2 || opcion.Key == ConsoleKey.NumPad2 || // verifico numpad 2 y 2 comun
+                opcion.Key == ConsoleKey.D3 || opcion.Key == ConsoleKey.NumPad3; // verifico numpad 3 y 3 comun
+        } 
+
+        private static void muestraRespuestaMenu(ConsoleKeyInfo opcion, string texto)
+        {
+            Console.Clear();
+            Console.Write("El resultado es: ");
+            switch (opcion.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    Console.Write(texto.ToUpper());
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    Console.Write(texto.ToLower());
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    Console.Write(texto.Length);
+                    break;
+            }
+        }
+    }
+}
---
+++ Unidad 2/LabSintaxis3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis3
+{
+    /*
+     1)	Crear una nueva aplicación de consola, llamada LabSintaxis3
+     2)	Crear un array de strings, las posiciones se determinarán por la variable cantIteraciones, un entero al que se le asignaran 5 posiciones. Luego, siempre se utilizara la misma variable
+     3)	Crear un ciclo for para ingresar cada una de las posiciones.
+
+     for (int i = 0; i < cantIteraciones; i++)
+     
+     4)	Crear un ciclo for para mostrar por pantalla cada una de las posiciones, una por línea, comenzando desde la última que se ingreso, hasta la primera.
+     5)	Ejecutar la aplicación.
+     
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones;
+            string[] valores;
+
+            cantIteraciones = obtenerCantidadIteraciones();
+
+            valores = rellenarArreglo(cantIteraciones);
+
+            muestraArrayInverso(valores);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Retorna cantidad de operaciones que quiere el cliente
+        /// </summary>
+        /// <returns>int</returns>
+        private static int obtenerCantidadIteraciones()
+        {
+            string input;
+            int cantIteraciones;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Escriba la cantidad de iteraciones: ");
+
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out _))
+                {
+                    Console.WriteLine("El valor ingresado debe ser un numero");
+                    Console.ReadKey();
+                }
+
+            } while (!int.TryParse(input, out cantIteraciones));
+
+            return cantIteraciones;
+        }
+
+        /// <summary>
+        /// Retorna el arreglo de string con sus respectivos valores
+        /// </summary>
+        /// <param name="iteraciones"></param>
+        /// <returns>string[]</returns>
+        private static string[] rellenarArreglo(int iteraciones)
+        {
+            string[] valoresIngresados = new string[iteraciones];
+
+            for (int i = 0; i < iteraciones; i++)
+            {
+                Console.Write("Ingrese un valor para el arreglo en la posicion " + i + ": ");
+                valoresIngresados[i] = Console.ReadLine();
+            }
+
+            return valoresIngresados;
+        }
+
+        /// <summary>
+        /// Muestra array inverso
+        /// </summary>
+        /// <param name="array"></param>
+        private static void muestraArrayInverso(string[] array)
+        {
+            for (int i = array.Length-1; i > -1; i--)
+            {
+                Console.WriteLine("Posicion: " + i + " valor: " + array[i]);
+            }
+        }
+    }
+}
