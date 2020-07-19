655816252033844ef4be08a572eac7626024fce3
Franco Villegas <villegasfranco1098@gmail.com>
2020-07-19 16:00:24 -0300

    unidad 3 lab 01

---
+++ Unidad3/Laboratorio01/Academia/FormMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Academia
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuSalir_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            FormLogin appLogin = new FormLogin();
+            if (appLogin.ShowDialog() != DialogResult.OK)
+            {
+                this.Dispose();
+            }
+        }
+    }
+
+}
