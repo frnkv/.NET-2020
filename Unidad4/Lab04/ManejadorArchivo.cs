045e8a41563413f5b899df708f25fe8fc10283a6
Franco Villegas <villegasfranco1098@gmail.com>
2020-09-08 23:09:28 -0300

    Lab 04 Unidad 4

---
+++ Unidad4/Lab04/ManejadorArchivo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+
+    class ManejadorArchivo
+    {
+        protected DataTable misContactos;
+
+        public ManejadorArchivo()
+        {
+            this.misContactos = this.getTabla();
+        }
+
+        public virtual DataTable getTabla()
+        {
+            return new DataTable();
+        }
+
+        public virtual void aplicaCambios()
+        {
+
+        }
+
+        public void Listar()
+        {
+            foreach (DataRow fila in this.misContactos.Rows)
+            {
+                if (fila.RowState != DataRowState.Deleted)
+                {
+                    foreach (DataColumn columna in this.misContactos.Columns)
+                    {
+                        Console.WriteLine("{0}: {1}", columna.ColumnName, fila[columna]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public void nuevaFila()
+        {
+            DataRow fila = this.misContactos.NewRow();
+
+            foreach (DataColumn col in this.misContactos.Columns)
+            {
+                Console.WriteLine("Ingrese {0}: ", col.ColumnName);
+
+                fila[col] = Console.ReadLine();
+            }
+
+            this.misContactos.Rows.Add(fila);
+        }
+
+        public void editarFila()
+        {
+            Console.WriteLine("Ingrese el numero de fila a editar");
+            int nroFila = int.Parse(Console.ReadLine());
+
+            DataRow fila = this.misContactos.Rows[nroFila - 1];
+
+            for (int nroCol = 1; nroCol < this.misContactos.Columns.Count; nroCol++)
+            {
+                DataColumn col = this.misContactos.Columns[nroCol];
+                Console.WriteLine("Ingrese {0}: ", col.ColumnName);
+
+                fila[col] = Console.ReadLine();
+            }
+        }
+
+        public void EliminarFila()
+        {
+            Console.WriteLine("Ingrese el numero de fila a eliminar");
+            int nroFila = int.Parse(Console.ReadLine());
+
+            this.misContactos.Rows[nroFila - 1].Delete();
+        }
+    }
+}
---
+++ Unidad4/Lab04/ManejadorArchivoXml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class ManejadorArchivoXml : ManejadorArchivo
+    {
+        protected DataSet ds;
+
+        public override DataTable getTabla()
+        {
+            this.ds = new DataSet();
+
+            this.ds.ReadXml("agenda.xml");
+
+            return this.ds.Tables["contactos"];
+        }
+
+        public override void aplicaCambios()
+        {
+            this.ds.WriteXml("agenda.xml");
+            this.ds.WriteXml("agendaconschema.xml", XmlWriteMode.WriteSchema);
+        }
+    }
+}
